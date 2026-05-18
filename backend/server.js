5fbfafa359d0e7d944ce12801afb2589862f6814
Bert154-beep <masabayan15@gmail.com>
2026-05-19 02:57:40 +0500

    new change

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: process.env.ORIGIN
 }))
 
 app.use("/register", async (req,res)=> {
